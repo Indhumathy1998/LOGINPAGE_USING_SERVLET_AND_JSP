dc7b353547e94c16403f57c17fe63efe52af6c98
Indhumathy1998 <73114841+Indhumathy1998@users.noreply.github.com>
2020-11-09 14:07:43 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,38 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+	  // LoginDao dao = new LoginDao();
+      //  if(dao.Check(uname, pass))
+       if(uname.equals("indhu") && pass.equals("mathy"))
+        {
+        	HttpSession session = request.getSession();
+        	session.setAttribute("username", "uname");
+        	response.sendRedirect("welcome.jsp");
+        }
+        else
+        {
+        	response.sendRedirect("login.jsp");
+        }
+	}
+
+}
